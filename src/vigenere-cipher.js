6dc5cff992d1ffd284eb9ff4a344c088ac7b4b20
Flame <5017_30@mail.ru>
2023-04-18 00:00:31 +0300

    9

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,75 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let encrypted = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const char = message[i];
+
+      if (char.match(/[A-Z]/)) {
+        const shift = key[j % key.length].charCodeAt(0) - 65;
+        const code = ((char.charCodeAt(0) - 65 + shift) % 26) + 65;
+        encrypted += String.fromCharCode(code);
+        j++;
+      } else {
+        encrypted += char;
+      }
+    }
+
+    return this.isDirect ? encrypted : encrypted.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let decrypted = '';
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      const char = encryptedMessage[i];
+
+      if (char.match(/[A-Z]/)) {
+        const shift = key[j % key.length].charCodeAt(0) - 65;
+        const code = ((char.charCodeAt(0) - 65 - shift + 26) % 26) + 65;
+        decrypted += String.fromCharCode(code);
+        j++;
+      } else {
+        decrypted += char;
+      }
+    }
+
+    return this.isDirect ? decrypted : decrypted.split('').reverse().join('');
   }
 }
 
